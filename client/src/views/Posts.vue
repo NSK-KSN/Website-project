29d8d1ddc92d7725bf1876b6187df961315a08a0
NSK-KSN <shadedknght@gmail.com>
2022-11-02 00:28:57 +0300

    cleaned up series page

--- client/src/views/Posts.vue
+++ client/src/views/Posts.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="posts">
+  <div>
     <div v-for="post in posts">
-    <div v-if="$route.params.id === post.malID ">
-    <p>
-      <h1>{{post.title}}</h1>
-      <span>{{post.malID}}</span>
-      <span><img :src="post.cover"/></span>
-      <span>Автор: {{post.author}}</span><br />
-      <span>Издатель: {{post.publisher}}</span><br />
-      <span>Описание:</span><br />
-      <span>{{post.description}}</span>
-    </p>
-    </div>
+      <div v-if="$route.params.id === post.malID ">
+        <section class="wrapper">
+          <div class="flex-col">
+            <img  class="flex-col--2" :src="post.cover" alt="cover">
+            <div class="flex-col--2" >
+              <h2>{{ post.title }}</h2>
+                <p>Автор: {{post.author}}</p>
+                <p>Издатель: {{post.publisher}}</p>
+                <br/>
+                <p>{{post.description}}</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -37,3 +40,26 @@
     }
   } 
 </script>
+
+<style>
+body {
+  background-color: #eee;
+}
+
+img {
+  height: 100%;
+  max-width: 400px;
+}
+
+.flex-col {
+  display: flex;
+  align-items: flex-start;
+  margin: 25px;
+  padding: 25px;
+}
+.flex-col--2 {
+  width: 50%;
+  margin: 50px
+}
+
+</style>
